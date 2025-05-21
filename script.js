15bcbf7af7ddecc5510aa4c3be18f2af92ab2fe9
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-22 03:00:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
+const list = document.getElementById('infi-list');
 
+function addInitialItems() {
+	for(let i=1;i<=10;i++){
+		const listItem = document.createElement('li');
+		listItem.textContent = `Item ${i}`;
+		list.appendChild(listItem);
+	}
+}
+window.onload = addInitialItems;
+
+let itemCount =11;
+function addMoreItems(){
+	for(let i=0;i<2;i++) {
+		const listItem = document.createElement('li');
+		listItem.textContent= `Item ${itemCount++}`;
+		list.appendChild(listItem);
+	}
+}
+
+window.onscroll= function(){
+	if(window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+		addMoreItems();
+	}
+};
